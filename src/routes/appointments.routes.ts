e189fc1ae23cc4561bc1504256778f8df2b6c904
Roger Oliveira <rogerpolvr@gmail.com>
2020-07-01 12:41:41 -0300

    [WIP] Criando um serviço para a criaçao de Appointments

--- src/routes/appointments.routes.ts
+++ src/routes/appointments.routes.ts
@@ -1,7 +1,8 @@
 import { Router } from 'express';
-import { startOfHour, parseISO } from 'date-fns';
+import { parseISO } from 'date-fns';
 
 import AppointmentsRepository from '../repositories/AppointmentsRepository';
+import CreateAppointmentService from '../services/CreateAppointmentService';
 
 const appointmentsRouter = Router();
 const appointmentsRepository = new AppointmentsRepository();
@@ -16,22 +17,9 @@ appointmentsRouter.get('/', (request, response) => {
 appointmentsRouter.post('/', (request, response) => {
   const { provider, date } = request.body;
 
-  const parsedDate = startOfHour(parseISO(date));
+  const parsedDate = parseISO(date);
 
-  const findAppointmentInSameDate = appointmentsRepository.findByDate(
-    parsedDate,
-  );
-
-  if (findAppointmentInSameDate) {
-    return response
-      .status(400)
-      .json({ message: 'This appointment is already booked' });
-  }
-
-  const appointment = appointmentsRepository.create({
-    provider,
-    date: parsedDate,
-  });
+  const appointment = new CreateAppointmentService({ provider, parsedDate });
 
   return response.json(appointment);
 });
