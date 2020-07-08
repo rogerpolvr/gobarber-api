8f825d9db47e191cfe8eb635e22e647d249c2235
Roger Oliveira <rogerpolvr@gmail.com>
2020-07-08 20:01:07 -0300

    Refatorando o AppointmentsRepository para utilizar o typeORM

--- src/repositories/AppointmentsRepository.ts
+++ src/repositories/AppointmentsRepository.ts
@@ -1,37 +1,13 @@
-import { isEqual } from 'date-fns';
+import { EntityRepository, Repository } from 'typeorm';
 import Appointment from '../models/Appointment';
 
-interface CreateAppointmentDTO {
-  provider: string;
-  date: Date;
-}
-
-class AppointmentsRepository {
-  private appointments: Appointment[];
-
-  constructor() {
-    this.appointments = [];
-  }
-
-  // Retorna todos os agendamentos
-  public all(): Appointment[] {
-    return this.appointments;
-  }
-
+@EntityRepository(Appointment)
+class AppointmentsRepository extends Repository<Appointment> {
   // Procura um agendamento na data solicitada
-  public findByDate(date: Date): Appointment | null {
-    const findAppointment = this.appointments.find(appointment =>
-      isEqual(date, appointment.date),
-    );
+  public async findByDate(date: Date): Promise<Appointment | null> {
+    const findAppointment = await this.findOne({ where: { date } });
 
     return findAppointment || null;
-  }
-
-  // Cria um novo agendamento
-  public create({ provider, date }: CreateAppointmentDTO): Appointment {
-    const appointment = new Appointment({ provider, date });
-    this.appointments.push(appointment);
-    return appointment;
   }
 }
 
