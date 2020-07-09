dd6e42d817c7413c404a4e12961f1678f2b6b22c
Roger Oliveira <rogerpolvr@gmail.com>
2020-07-08 22:32:11 -0300

    Atualizando as migrations

--- src/database/migrations/1594088757495-CreateAppointments.ts
+++ src/database/migrations/1594088757495-CreateAppointments.ts
@@ -16,12 +16,21 @@ export class CreateAppointments1594088757495 implements MigrationInterface {
           {
             name: 'provider',
             type: 'varchar',
-            isNullable: false,
           },
           {
             name: 'date',
             type: 'timestamp with time zone',
             isNullable: false,
+          },
+          {
+            name: 'created_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+          {
+            name: 'updated_at',
+            type: 'timestamp',
+            default: 'now()',
           },
         ],
       }),
--- src/database/migrations/1594252625439-CreateUsers.ts
+++ src/database/migrations/1594252625439-CreateUsers.ts
@@ -23,6 +23,10 @@ export default class CreateUsers1594252625439 implements MigrationInterface {
             isUnique: true,
           },
           {
+            name: 'password',
+            type: 'varchar',
+          },
+          {
             name: 'created_at',
             type: 'timestamp',
             default: 'now()',
