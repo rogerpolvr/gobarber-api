3b79982719257850c796b63a74860976e66e28ca
Roger Oliveira <rogerpolvr@gmail.com>
2020-07-13 21:54:13 -0300

    Implementando o Upload de Avatar para os usuários

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,8 +1,14 @@
 import { Router } from 'express';
+import multer from 'multer';
+import uploadConfig from '../config/upload';
 
 import CreateUserService from '../services/CreateUserService';
+import UpdateUserAvatarService from '../services/UpdateUserAvatarService';
+
+import ensureAuthenticated from '../middlewares/ensureAuthenticated';
 
 const usersRouter = Router();
+const upload = multer(uploadConfig);
 
 // POST: User
 usersRouter.post('/', async (request, response) => {
@@ -25,5 +31,29 @@ usersRouter.post('/', async (request, response) => {
     return response.status(400).json({ error: err.message });
   }
 });
+
+// O middle é colocado diretamente na rota pois a rota de criação
+// não exige que o usuário esteja autenticado. (Faz sentido né?)
+usersRouter.patch(
+  '/avatar',
+  ensureAuthenticated,
+  upload.single('avatar'),
+  async (request, response) => {
+    try {
+      const updateUserAvatar = new UpdateUserAvatarService();
+
+      const user = await updateUserAvatar.execute({
+        user_id: request.user.id,
+        avatarFilename: request.file.filename,
+      });
+
+      delete user.password;
+
+      return response.json(user);
+    } catch (err) {
+      return response.status(400).json({ error: err.message });
+    }
+  },
+);
 
 export default usersRouter;
--- src/server.ts
+++ src/server.ts
@@ -1,24 +1,21 @@
 import 'reflect-metadata';
+
 import express from 'express';
 import routes from './routes';
+import uploadConfig from './config/upload';
+
 import './database';
 
 const app = express();
 
 app.use(express.json());
+app.use('/files', express.static(uploadConfig.directory));
 app.use(routes);
 
-app.post('/users', (request, response) => {
-  const { name, email } = request.body;
-
-  const user = {
-    name,
-    email,
-  };
-
-  return response.json(user);
-});
-
 app.listen(3333, () => {
-  console.log('\x1b[5m\x1b[33m%s\x1b[0m\x1b[1m', '🚀 Server started on localhost port 3333! 🚀', '\x1b[0m');
+  console.log(
+    '\x1b[5m\x1b[33m%s\x1b[0m\x1b[1m',
+    '🚀 Server started on localhost port 3333! 🚀',
+    '\x1b[0m',
+  );
 });
